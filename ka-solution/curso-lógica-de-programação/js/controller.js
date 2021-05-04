f87581cdb97e710f4d2b38a7f3a434d96fcb1032
Moyses Junior <moyses_junior@hotmail.com>
2021-05-04 19:45:02 -0300

    Initial commit

---
+++ ka-solution/curso-lógica-de-programação/js/controller.js
@@ -0,0 +1,10 @@
+var botao = document.getElementById("calcular");
+botao.addEventListener('click', function() {var paciente = lePaciente();)
+mostraPaciente(paciente)});
+
+/*
+console.log(nome);
+console.log(`Peso: ${peso} - Altura: ${altura}`);
+console.log(`Calculo do IMC: ${imc.toFixed(2)}`);
+*/
+
